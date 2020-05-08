ccbdf3991333c2fb5305095854a753269a4e2423
DevYojan <devyojan@gmail.com>
2020-05-08 05:07:27 +0400

    uibuild

---
+++ README.md
@@ -0,0 +1,2 @@
+Find the api online
+https://fierce-plateau-69676.herokuapp.com/
---
+++ build/precache-manifest.66f0c77245e515defebdaee1fe8b10b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af811e5e610cdf2041bf4d99ca381eac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a4742abb4fdfce15263",
+    "url": "/static/css/main.b04d46a8.chunk.css"
+  },
+  {
+    "revision": "c304a1bb1127a3725e08",
+    "url": "/static/js/2.411224bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.411224bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4742abb4fdfce15263",
+    "url": "/static/js/main.45802546.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
